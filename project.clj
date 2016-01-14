1c7b65a1df552eccc677de05160cff55131c3f57
Vivek Prahlad <vivek.prahlad@gmail.com>
2016-01-15 09:49:56 +1100

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject diamond "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "TW coding challenge - generates an ASCII diamond"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.cli "0.3.3"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :aot [diamond.core]
   :main diamond.core)
