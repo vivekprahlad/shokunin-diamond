1cce3a43ab995cddcf2b8740734b87a6fd095e15
Vivek Prahlad <vivek.prahlad@gmail.com>
2016-01-15 09:59:30 +1100

    Detect whether leiningen is installed

--- go.sh
+++ go.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+if ! [ -x "$(command -v lein)" ]; then
+    echo 'Please install leiningen from http://leiningen.org/'
+    exit
+fi
+
 if [ $# -eq 0 ]; then
     echo "Usage:"
     echo "Run program: ./go.sh run <letter>"
